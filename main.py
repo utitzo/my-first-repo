092b3b345e5b1056b5a363d829872abab01a1e01
Ohad Utitz <utitzo@gmail.com>
2025-06-23 15:18:44 +0300

    change student details

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Alex", 30, "English", 3 ,85)
+student = Student("mike", 22, "English", 3 ,90)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
